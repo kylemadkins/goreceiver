074a3de964518683612f6a784bafe57ee5de803e
Kyle Adkins <kyle.m.adkins@gmail.com>
2022-06-21 02:43:12 -0500

    Add tests

---
+++ goreceiver_test.go
@@ -0,0 +1,61 @@
+/*
+Summary:
+Copy your rcv-func solution to this directory and write unit tests.
+
+Requirements:
+Write unit tests that ensure:
+- Health & energy can not go above their maximums
+- Health & energy can not go below 0
+
+If any of your  tests fail, make the necessary corrections
+in the copy of your solution file.
+
+*/
+
+package main
+
+import "testing"
+
+func newPlayer() Player {
+	return Player{100, 100, 100, 100, "Test"}
+}
+
+func TestSetHealth(t *testing.T) {
+	p := newPlayer()
+	p.setHealth(p.maxHealth + 100)
+	if p.health > p.maxHealth {
+		t.Fatalf(
+			"Health exceeded limit. Expected %v, not %v",
+			p.maxHealth,
+			p.health,
+		)
+	}
+	p.setHealth(20)
+	if p.health != 20 {
+		t.Fatalf(
+			"Health incorrect. Expected %v, not %v",
+			20,
+			p.health,
+		)
+	}
+}
+
+func TestSetEnergy(t *testing.T) {
+	p := newPlayer()
+	p.setEnergy(p.maxEnergy + 100)
+	if p.energy > p.maxEnergy {
+		t.Fatalf(
+			"Energy exceeded limit. Expected %v, not %v",
+			p.maxEnergy,
+			p.health,
+		)
+	}
+	p.setEnergy(20)
+	if p.energy != 20 {
+		t.Fatalf(
+			"Energy incorrect. Expected %v, not %v",
+			20,
+			p.energy,
+		)
+	}
+}
